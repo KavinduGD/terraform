8c6b9fe1ac8796e758d443bb9b044c7e5025c352
Kavindu Gihan <kavidudharmasiri90@gmail.com>
2025-10-17 23:27:22 +0530

    s3 static website project half complete

---
+++ 04-resources/02-static-web-hosting/s3.tf
@@ -0,0 +1,73 @@
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "prod-s3-tkg-2025"
+
+  tags = {
+    Name        = "Static Web Hosting Bucket"
+    Environment = "Production"
+  }
+}
+
+
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+
+data "aws_iam_policy_document" "allow_public_read" {
+  statement {
+    sid    = "PublicReadGetObject"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.s3_bucket.arn}/*"]
+  }
+}
+
+resource "aws_s3_bucket_policy" "public_read_policy" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  policy = data.aws_iam_policy_document.allow_public_read.json
+
+  depends_on = [aws_s3_bucket_public_access_block.public_access]
+}
+
+resource "aws_s3_bucket_website_configuration" "example" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+
+# Upload index.html
+resource "aws_s3_object" "index_html" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  key    = "index.html"
+  source = "index.html"
+  content_type = "text/html"
+#   acl    = "public-read"
+}
+
+# Upload error.html (optional)
+# resource "aws_s3_object" "error_html" {
+#   bucket = aws_s3_bucket.s3_bucket.id
+#   key    = "error.html"
+#   source = "error.html"
+#   content_type = "text/html"
+#   acl    = "public-read"
+# }
